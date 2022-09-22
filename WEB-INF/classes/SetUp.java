9132f762b12e915babe51f61d858c17f385ad261
Melanie Ta <nngoc2701@gmail.com>
2022-09-22 12:35:42 -0700

    Update SetUp

--- WEB-INF/classes/SetUp.java
+++ WEB-INF/classes/SetUp.java
@@ -1,39 +1,69 @@
 import java.sql.*;
 
 public class SetUp {
-    private SetUp() {
+    public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (Exception ex) {
-            System.out.println("Message: " + ex.getMessage());
+            System.out.println("SetUp/Message: " + ex.getMessage());
         }
-    }
-    public static void main(String[] args) {
         try {
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306", "comp3940", "");
 
             Statement s = con.createStatement();
-            s.executeUpdate("create schema if not exists comp3940;");
-            s.executeUpdate("use comp3940;");
-            s.executeUpdate("drop table if exists Photos;");
-            s.executeUpdate("drop table if exists Users;");
-            s.executeUpdate("create table Users (" +
+            s.executeUpdate("CREATE SCHEMA IF NOT EXISTS comp3940;");
+            s.executeUpdate("USE comp3940;");
+            s.executeUpdate("DROP TABLE IF EXISTS Photos;");
+            s.executeUpdate("DROP TABLE IF EXISTS Users;");
+            s.executeUpdate("CREATE TABLE Users (" +
                     "id VARBINARY(16)," +
-                    "userId varchar(50) unique," +
-                    "password varchar(50)," +
+                    "userId VARCHAR(50) UNIQUE," +
+                    "password VARCHAR(255)," +
                     "PRIMARY KEY (id));");
-            s.executeUpdate("create table Photos(" +
+            s.executeUpdate("CREATE TABLE Photos(" +
                     "id VARBINARY(16)," +
                     "userId VARBINARY(16)," +
                     "picture MEDIUMBLOB," +
-                    "fileName varchar(50)," +
-                    "caption char(100)," +
-                    "dateTaken date," +
+                    "fileName VARCHAR(50)," +
+                    "caption CHAR(100)," +
+                    "dateTaken DATE," +
                     "PRIMARY KEY (id)," +
                     "FOREIGN KEY (userid) REFERENCES Users(id));");
             s.close();
         } catch (Exception e) {
-            System.out.println("Message: " + e.getMessage());
+            System.out.println("SetUp/Message: " + e.getMessage());
+        }
+    }
+
+    public static void setUpTable() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (Exception ex) {
+            System.out.println("SetUp/Message: " + ex.getMessage());
+        }
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306", "comp3940", "");
+
+            Statement s = con.createStatement();
+            s.executeUpdate("CREATE SCHEMA IF NOT EXISTS comp3940;");
+            s.executeUpdate("USE comp3940;");
+            s.executeUpdate("CREATE TABLE IF NOT EXISTS Users (" +
+                    "id VARBINARY(16)," +
+                    "userId VARCHAR(50) UNIQUE," +
+                    "password VARCHAR(255)," +
+                    "PRIMARY KEY (id));");
+            s.executeUpdate("CREATE TABLE IF NOT EXISTS Photos(" +
+                    "id VARBINARY(16)," +
+                    "userId VARBINARY(16)," +
+                    "picture MEDIUMBLOB," +
+                    "fileName VARCHAR(50)," +
+                    "caption CHAR(100)," +
+                    "dateTaken DATE," +
+                    "PRIMARY KEY (id)," +
+                    "FOREIGN KEY (userid) REFERENCES Users(id));");
+            s.close();
+        } catch (Exception e) {
+            System.out.println("SetUp/Message: " + e.getMessage());
         }
     }
 }
